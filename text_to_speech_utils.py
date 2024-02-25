5c4e214d75308b841bce2c5a19e9fe94b337264e
Swapnil Patel <swapnilpatel95@gmail.com>
2024-02-24 20:24:40 -0500

    xml stuff

--- text_to_speech_utils.py
+++ text_to_speech_utils.py
@@ -5,9 +5,26 @@ import os
 import azure.cognitiveservices.speech as speechsdk
 import xml.etree.ElementTree as ET
 import difflib
+import enum
+from typing import List
+import dataclasses
+
+
+class VoiceStyle(enum.Enum):
+    ANGRY = 'angry'
+    CHEERFUL = 'cheerful'
+    SAD = 'sad'
+
+@dataclasses.dataclass
+class StyledPhrase:
+    phrase: str
+    voice_style: VoiceStyle
+    intensity: int  # 0.01 - 2
+
 priv_key = open('priv_key.txt', 'r')
 priv_key = priv_key.read()
 service_region = "eastus"
+
 
 
 class TextEmotion(object):
@@ -18,6 +35,55 @@ class TextEmotion(object):
         return self.text
     def get_emotion(self):
         return self.emotion
+
+def xml_builder(voice_name: str, phrases: List[StyledPhrase]):
+    express_as_elems = []
+    for phrase in phrases:
+        express_as_elems = (
+            f'<mstts:express-as style="{phrase.voice_style.value}" styledegree="{phrase.style_intensity.value}">'
+            f'{phrase.phrase}'
+            f'</mstts:express-as>'
+        )
+
+    combined_phrases = f''.join(express_as_elems)
+    return (
+        f'<speak version="1.0" xmlns="http://www.w3.org/2001/10/synthesis" xmlns:mstts="https://www.w3.org/2001/mstts" xml:lang="en-US">\n'
+        f'<voice name="{voice_name}">\n'
+        f'{combined_phrases}'
+        f'</voice>\n'
+        f'</speak>\n'
+    )
+
+
+def parse_input(raw_input: str):
+    if '/a' in raw_input:
+        angry_index = raw_input.find('/a')
+        mod_str = raw_input[angry_index:angry_index+raw_input[angry_index:].find(' ')]
+        mod_str = mod_str.strip()
+        mod_str = mod_str[2:]
+        try:
+            intensity = int(mod_str)
+            intensity = min(2.0, max(0.01, intensity))
+            return VoiceStyle.ANGRY, intensity, raw_input.replace('/a', '')
+        except ValueError:
+            return VoiceStyle.ANGRY, 1.0, raw_input.replace('/a', '')
+        
+    if '/d' in raw_input:
+        angry_index = raw_input.find('/d')
+        mod_str = raw_input[angry_index:angry_index+raw_input[angry_index:].find(' ')]
+        mod_str = mod_str.strip()
+        mod_str = mod_str[2:]
+        try:
+            intensity = int(mod_str)
+            intensity = min(2.0, max(0.01, intensity))
+            return VoiceStyle.SAD, intensity, raw_input.replace('/d', '')
+        except ValueError:
+            return VoiceStyle.SAD, 1.0, raw_input.replace('/d', '')
+        
+    else:
+        return VoiceStyle.CHEERFUL, 1.0, raw_input.replace('/d', '')
+        
+
 
 
 class BeMyVoice(object):
